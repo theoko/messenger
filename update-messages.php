d8155a8c3e6d809376a6f77bd5e32abf8248f450
theoko <konstantopoulostheodore@gmail.com>
2015-09-05 14:25:29 +0300

    Files

---
+++ update-messages.php
@@ -0,0 +1,17 @@
+<?php
+$db = new mysqli("localhost", "root", "1362im5km6jpitho", "messenger");
+
+if ($db->connect_error) {
+	die("Sorry, there was a problem connecting to our database.");
+}
+
+$username = stripslashes(htmlspecialchars($_GET['username']));
+$message = stripslashes(htmlspecialchars($_GET['message']));
+
+if ($message == "" || $username == "") {
+	die();
+}
+
+$result = $db->prepare("INSERT INTO messages VALUES('',?,?)");
+$result->bind_param("ss", $username, $message);
+$result->execute();
