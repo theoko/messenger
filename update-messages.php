62b831ffbaa8f7bd70d1683e3c78318cc86eb56f
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2015-09-05 14:34:36 +0300

    Update update-messages.php

--- update-messages.php
+++ update-messages.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli("localhost", "root", "1362im5km6jpitho", "messenger");
+$db = new mysqli("localhost", "root", "password", "messenger");
 
 if ($db->connect_error) {
 	die("Sorry, there was a problem connecting to our database.");
